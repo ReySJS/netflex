50225346acbfba44ba48e23570f042107f988ae1
Rey <rey.filareto@outlook.com>
2022-04-13 12:21:58 -0300

    Update axios branch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { HomeMovieThumbnail } from '../../components/HomeMovieThumbnail'
+import { api } from '../../services/api'
 
 import * as S from './styles'
+
+interface HandleMovieTypes {
+  name: string
+  title: string
+  subtitle: string
+}
+
+interface MovieTypes {
+  name: string
+  title: string
+  subtitle: string
+}
 
 /**
  * Archive: src/pages/Home/index.tsx
@@ -14,17 +27,21 @@ import * as S from './styles'
  */
 
 export const Home = () => {
+  const [movies, setMovies] = useState<MovieTypes[]>([])
   const [selectedMovie, setSelectedMovie] = useState('')
   const [movieTitle, setMovieTitle] = useState('(DES)ENCANTO')
   const [movieSubtitle, setMovieSubtitle] = useState(
     'Toda princesa tem seus deveres, mas ela quer mesmo é encher a cara. E com um elfo e um demônio como parceiros, levar o rei à loucura será uma tarefa fácil.',
   )
 
-  interface HandleMovieTypes {
-    name: string
-    title: string
-    subtitle: string
-  }
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await api.get('/movies')
+      setMovies(data)
+      console.log(data)
+    }
+    getData()
+  }, [])
 
   const handleMovie = ({ name, title, subtitle }: HandleMovieTypes) => {
     setSelectedMovie(name)
@@ -37,50 +54,24 @@ export const Home = () => {
       <S.Banner name={selectedMovie}>
         <S.Title>{movieTitle}</S.Title>
         <S.Subtitle>{movieSubtitle}</S.Subtitle>
+        <button onClick={() => setSelectedMovie('reload')}>Recarregar</button>
       </S.Banner>
 
       <S.Carrousel>
         <S.PageTitle>Minha Lista</S.PageTitle>
-        <HomeMovieThumbnail
-          name="adam"
-          onClick={() =>
-            handleMovie({
-              name: 'adam',
-              title: 'Nome qualquer',
-              subtitle: 'Adam Subtitle',
-            })
-          }
-        />
-        <HomeMovieThumbnail
-          name="matrix"
-          onClick={() =>
-            handleMovie({
-              name: 'matrix',
-              title: 'Matrix Title',
-              subtitle: 'Matrix Subtitle',
-            })
-          }
-        />
-        <HomeMovieThumbnail
-          name="loki"
-          onClick={() =>
-            handleMovie({
-              name: 'loki',
-              title: 'Loki Title',
-              subtitle: 'Loki Subtitle',
-            })
-          }
-        />
-        <HomeMovieThumbnail
-          name="batman"
-          onClick={() =>
-            handleMovie({
-              name: 'batman',
-              title: 'Batman Title',
-              subtitle: 'Batman Subtitle',
-            })
-          }
-        />
+
+        {movies.map((movie) => (
+          <HomeMovieThumbnail
+            name={movie.name}
+            onClick={() =>
+              handleMovie({
+                name: movie.name,
+                title: movie.title,
+                subtitle: movie.subtitle,
+              })
+            }
+          />
+        ))}
       </S.Carrousel>
     </S.Conteiner>
   )
