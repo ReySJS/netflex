62cebea2f815d5d48ba6810b356a0cf6b49f9cf9
Rey <rey.filareto@outlook.com>
2022-06-20 20:41:05 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_URL,
+  baseURL: 'http://localhost:3030',
   withCredentials: false,
   headers: { 'Access-Control-Allow-Origin': '*' },
 });
